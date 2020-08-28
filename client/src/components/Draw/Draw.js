31d7539fc03f975cb4bc740226eb9e2b77cf3479
mrenadams <marissaa@mendwell.com>
2020-08-27 18:25:15 -0700

    init

---
+++ client/src/components/Draw/Draw.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Draw.css";
+
+function Draw() {
+    return (
+        <div>
+            Hello this is works
+        </div>
+    )
+}
+
+export default Draw;
