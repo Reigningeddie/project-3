9ae8e7ddc85a79efbdca2fbde9c2e58f81005019
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-29 12:57:33 -0700

    modified server with http module

--- server.js
+++ server.js
@@ -7,32 +7,34 @@ const initDb = require("./config/initDb");
 const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
 const errorMiddleware = require("./routes/errorMiddleware");
+const http = require("http");
+const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3001;
 
 // log all requests to the console in development
 if (process.env.NODE_ENV !== "production") {
-  app.use(morgan("dev"));
+  server.use(morgan("dev"));
 }
 
 // Setting up express to use json and set it to req.body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 initDb();
 
 // Serve up static assets in production (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  server.use(express.static("client/build"));
 }
 
-app.use(authRouter, usersRouter, errorMiddleware);
+server.use(authRouter, usersRouter, errorMiddleware);
 
 // Send all other requests to react app
-app.get("*", (req, res) => {
+server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
