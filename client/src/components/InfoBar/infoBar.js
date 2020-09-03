64d421f939f858813ff7f8b62b344eac0e455776
joey <joseph66magdaleno@yahoo.com>
2020-09-03 14:31:48 -0700

    created /join and /chat feature to the app

---
+++ client/src/components/InfoBar/infoBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from '../../icons/onlineIcon.png';
+
+
+import './infoBar.css';
+
+const InfoBar = ({room}) => (
+    <div className="infoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online image"/>
+            <h3>{room}</h3>
+
+        </div>
+        <div className="rightInnerContainer">
+            <a href="/join" ><img src={closeIcon} alt="close image"/></a>
+            
+        </div>
+    </div>
+
+)
+
+export default InfoBar;
