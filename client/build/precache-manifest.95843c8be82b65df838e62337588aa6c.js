9d89b3331d813f5777cf1f1784d5829340246e71
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-09-17 17:05:24 -0700

    build folder

---
+++ client/build/precache-manifest.95843c8be82b65df838e62337588aa6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfd03a8f3bda66ffbb8fa3f40cd7019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9f387a74300359ed453",
+    "url": "/static/css/main.ff837302.chunk.css"
+  },
+  {
+    "revision": "abe82262ecd6d22926d1",
+    "url": "/static/js/2.0fbe409b.chunk.js"
+  },
+  {
+    "revision": "2d56df8f373e6d26deaea8d554a9bf0f",
+    "url": "/static/js/2.0fbe409b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f387a74300359ed453",
+    "url": "/static/js/main.fbadae24.chunk.js"
+  },
+  {
+    "revision": "89871a5315cf0575417b",
+    "url": "/static/js/runtime-main.1480d077.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
